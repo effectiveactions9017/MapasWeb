cce345c0b21b6f61cbd867678b24029df6a710a3
effectiveactions9017 <ceo@effectiveactions.us>
2025-07-16 11:04:34 -0500

    Update index.js

--- ANZA/mapEvConst/src/index.js
+++ ANZA/mapEvConst/src/index.js
@@ -2,7 +2,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZWZmZWN0aXZlYWN0aW9uczkwMTciLCJhIjoiY21iOWY1eGtiMGQ2cjJqcG9xbTRjZnQxMiJ9.8p55iS2R45-p8lxTerDL9Q';
 const map = new mapboxgl.Map({
     style: 'mapbox://styles/mapbox/dark-v11',
-    center: [-75.8542, 6.3036], 
+    center: [-75.53982,  6.303375], 
     zoom: 14,
     pitch: 0,
     bearing: 0,
@@ -62,7 +62,7 @@ map.on('style.load', () => {
             map.on('mousemove', 'buildings', (e) => {
                 const feature = e.features[0];
                 const year = feature.properties.const_year;
-                const area = feature.properties.area_in_meters;
+                const area = feature.properties.area_in_me;
                 const areaRedondeada = Math.round(area, 0);
 
                 const popupContent = 
