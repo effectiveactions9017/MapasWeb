ebfb562f7f65a5c37632a3a1fe923527720d851c
effectiveactions9017 <ceo@effectiveactions.us>
2025-11-13 17:30:20 -0500

    index.js

--- mapAcCatastro/src/index.js
+++ mapAcCatastro/src/index.js
@@ -1,3 +1,27 @@
+// Usar token propio de Mapbox
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9yZ2VwYXRpbm8iLCJhIjoiY2tnc2R0c20zMWVvdTJ5bXRpZ3Z4bDN1dCJ9.2LgsqgR7lXR6YFH2IaNc-w';
+
+const map = new mapboxgl.Map({
+    style: 'mapbox://styles/mapbox/dark-v11',
+    center: [-76.62000, 7.88400],
+    zoom: 14,
+    pitch: 0,
+    bearing: 0,
+    container: 'map',
+    antialias: true
+});
+
+// Popup general
+let popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false,
+    className: 'custom-popup'
+});
+
+// ------------------------
+//    CLASIFICACIÓN
+// ------------------------
+
 map.on('style.load', () => {
 
     // Fuente: Clasificación Plusvalía
@@ -6,7 +30,7 @@ map.on('style.load', () => {
         data: '../src/data/Clasificación_Plusvalia.geojson'
     });
 
-    // Capa categorizada por CLASIFICACION_PLUSVALIA
+    // Layer categorizado según CLASIFICACION_PLUSVALIA
     map.addLayer({
         id: 'layer_plusvalia',
         source: 'c_plusvalia',
@@ -16,29 +40,27 @@ map.on('style.load', () => {
                 'match',
                 ['get', 'CLASIFICACION_PLUSVALIA'],
 
-                'Muy Alta', '#e74c3c',   // rojo
-                'Alta',     '#e67e22',   // naranja
-                'Media',    '#f1c40f',   // amarillo
-                'Baja',     '#2ecc71',   // verde
-                'Muy Baja', '#3498db',   // azul
+                'Muy Alta', '#e74c3c',      // rojo
+                'Alta',     '#e67e22',      // naranja
+                'Media',    '#f1c40f',      // amarillo
+                'Baja',     '#2ecc71',      // verde
+                'Muy Baja', '#3498db',      // azul
 
-                '#bdc3c7'  // por defecto
+                '#bdc3c7'   // color default si no coincide
             ],
             'fill-opacity': 0.75,
             'fill-outline-color': '#ffffff'
         }
     });
 
-    // POPUP dinámico
+    // POPUP
     map.on('mousemove', 'layer_plusvalia', (e) => {
-        const f = e.features[0];
-
-        const clasif = f.properties.CLASIFICACION_PLUSVALIA;
+        const feature = e.features[0];
 
         popup
             .setLngLat(e.lngLat)
             .setHTML(`
-                <strong>Clasificación: </strong> ${clasif}<br>
+                <strong>Clasificación:</strong> ${feature.properties.CLASIFICACION_PLUSVALIA}<br>
                 <a style="font-size:9px;">&#9400 EffectiveActions</a>
             `)
             .addTo(map);
