046fdb4250b4391caf92d08da67e335386acc914
effectiveactions9017 <ceo@effectiveactions.us>
2026-01-19 17:32:48 -0500

    Update main.js

--- SESQUILE_BOSQUE/src/main.js
+++ SESQUILE_BOSQUE/src/main.js
@@ -1,17 +1,10 @@
-// ===============================
-// Mapbox – Sesquilé | Cambio Bosques
-// ===============================
-
 mapboxgl.accessToken =
   'pk.eyJ1IjoiZWZmZWN0aXZlYWN0aW9uczkwMTciLCJhIjoiY21raGliOTM1MGl3ejNkb25kOWF6ZzRleCJ9.S_kG8hu35MYRvWrNyKdfWA';
 
-// Tileset IDs (sin mapbox://)
+// Tileset IDs (raster)
 const TILESET_2017 = 'effectiveactions9017.bosques_2017_esriLC_color-7e6vbh';
 const TILESET_2024 = 'effectiveactions9017.bosques_2024_esriLC_color-7dr8hx';
 
-// -------------------------------
-// Mapas
-// -------------------------------
 const beforeMap = new mapboxgl.Map({
   container: 'before',
   style: 'mapbox://styles/mapbox/light-v11',
@@ -28,57 +21,40 @@ const afterMap = new mapboxgl.Map({
   maxZoom: 14
 });
 
-// -------------------------------
-// Utils
-// -------------------------------
 function addRaster(map, id, tileset) {
   if (map.getSource(id)) return;
 
   map.addSource(id, {
     type: 'raster',
     tiles: [
-      `https://api.mapbox.com/v4/${tileset}/{z}/{x}/{y}.png?access_token=${mapboxgl.accessToken}`
+      `https://api.mapbox.com/v4/${tileset}/{z}/{x}/{y}.pngraw?access_token=${mapboxgl.accessToken}`
     ],
     tileSize: 256,
     maxzoom: 14
   });
 
   map.addLayer({
-    id: id + '-layer',
+    id: `${id}-layer`,
     type: 'raster',
     source: id,
-    paint: { 'raster-opacity': 0.9 }
+    paint: {
+      'raster-opacity': 1,
+      'raster-contrast': 0.4,
+      'raster-saturation': 0.8
+    }
   });
 }
 
-// -------------------------------
-// Cargar raster (FORMA CORRECTA)
-// -------------------------------
-let loaded1 = false;
-let loaded2 = false;
+let a = false, b = false, compare;
 
 beforeMap.on('load', () => {
   addRaster(beforeMap, 'bosques2017', TILESET_2017);
-  loaded1 = true;
-  initCompare();
+  a = true;
+  if (a && b && !compare) compare = new mapboxgl.Compare(beforeMap, afterMap, '#comparison-container');
 });
 
 afterMap.on('load', () => {
   addRaster(afterMap, 'bosques2024', TILESET_2024);
-  loaded2 = true;
-  initCompare();
+  b = true;
+  if (a && b && !compare) compare = new mapboxgl.Compare(beforeMap, afterMap, '#comparison-container');
 });
-
-// -------------------------------
-// Swipe
-// -------------------------------
-let compare;
-function initCompare() {
-  if (loaded1 && loaded2 && !compare) {
-    compare = new mapboxgl.Compare(
-      beforeMap,
-      afterMap,
-      '#comparison-container'
-    );
-  }
-}
